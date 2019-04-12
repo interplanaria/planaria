da6f78e04300620ebe2e4dbe6bd53e48a2adb033
unwriter <unwriter@protonmail.com>
2019-04-12 17:45:41 -0400

    no need for sudo
    windows doesn't have sudo command

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ if [[ "$1" == "start" ]]; then
   echo "arg1: $3"
   echo "arg2: $4"
   echo "arg3: $5"
-  sudo mongod --bind_ip_all --wiredTigerCacheSizeGB=$2 &
+  mongod --bind_ip_all --wiredTigerCacheSizeGB=$2 &
   until nc -z localhost 27017
     do
       sleep 1
