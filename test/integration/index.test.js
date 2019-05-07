ab4030e940861fc8fb1f5ac9193ae3d7d4e61f4b
ppetzold <ppetzold89@gmail.com>
2019-05-08 07:46:52 +0800

    fix small issues

--- test/integration/index.test.js
+++ test/integration/index.test.js
@@ -2,7 +2,7 @@ const assert = require('assert')
 
 describe('planaria', () => {
   describe('start', () => {
-    it('should fail to start', async () => {
+    it('should fail to start', () => {
       assert.throws(() => require('../..'))
     })
   })
