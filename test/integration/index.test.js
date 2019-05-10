ffa7085398ed00dfd2d4cdc2d92afce467b5eb8d
ppetzold <ppetzold89@gmail.com>
2019-05-10 14:46:53 +0800

    Use simple assertion for dummy tests

--- test/integration/index.test.js
+++ test/integration/index.test.js
@@ -1,9 +1,11 @@
+'use strict'
+
 const assert = require('assert')
 
 describe('planaria', () => {
   describe('start', () => {
-    it('should fail to start', () => {
-      assert.throws(() => require('../..'))
+    it('1 + 1 should be 2', async () => {
+      assert.strictEqual(1 + 1, 2)
     })
   })
 })
